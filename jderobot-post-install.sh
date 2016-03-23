6f3321a24f96173d35dee3c9dc6a585f48fbd760
Victor Arribas <v.arribas.urjc@gmail.com>
2016-03-23 21:12:11 +0100

    include environment injection (into .bashrc)

--- jderobot-post-install.sh
+++ jderobot-post-install.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+#
+# Copyright (c) 2015-2016
+# Author: Victor Arribas <v.arribas.urjc@gmail.com>
+# License: GPLv3 <http://www.gnu.org/licenses/gpl-3.0.html>
+
 
 # This script injects JdeRobot requirements into Gazebo user settings.
 # This step should be done *once* installed. But also each time you
@@ -15,3 +20,10 @@ cp -r /usr/local/share/jderobot/gazebo/models ~/.gazebo
 
 # Add custom Worlds
 cp -r /usr/local/share/jderobot/gazebo/worlds ~/.gazebo
+
+
+# Inject Gazebo into environment (.bashrc)
+grep -q 'gazebo-setup.sh' ~/.bashrc || cat<<EOF>>~/.bashrc
+test -z "\$GAZEBO_MASTER_URI" \\
+  && source /usr/local/share/jderobot/gazebo/gazebo-setup.sh
+EOF
